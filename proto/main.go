078d3a4a70d2b5b246e4ed8cc892867efcfa948e
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-03-03 22:32:46 -0600

    proto system for internal commands

---
+++ proto/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+	"errors"
+)
+
+//private types
+type (
+	wr_t struct{
+		stdin io.Reader
+		stdout io.Writer
+		stderr io.Writer
+	}
+)
+
+type (
+	Fn func(exec *Executor, args []string) (int)
+	Executor struct {
+		fns map[string]Fn
+		stdin io.Reader
+		stdout io.Writer
+		stderr io.Writer
+		wr wr_t
+	}
+)
+
+var (
+	CmdNotFound = errors.New("unrecognized command") 
+)
+
+//private values
+var (
+	wr wr_t
+)
+
+func main() {
+	fns := Default_fns()
+	exec := Init(fns, os.Stdin, os.Stdout, os.Stderr)
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "not enough args (usage: [cmd] [args...])")
+		return
+	}
+	ret, e := exec.Exec(os.Args[1:])
+	if e != nil { fmt.Fprintf(os.Stderr, "%v\n", e) }
+	fmt.Printf("\nreturn code: %d\n", ret)
+}
+
+func Default_fns() map[string]Fn {
+	return map[string]Fn {
+		"date": date,
+	}
+}
+
+func Init(
+	fns map[string]Fn,
+	stdin io.Reader,
+	stdout io.Writer,
+	stderr io.Writer,
+) Executor {
+	return Executor {
+		fns: fns,
+		stdout: stdout,
+		stderr: stderr,
+		stdin: stdin,
+		wr: wr_t {
+			stdout: stdout,
+			stderr: stderr,
+			stdin: stdin,
+		},
+	}
+}
+
+func (exec Executor) Exec(args []string) (int, error) {
+	if exec.fns[args[0]] != nil { 
+		return exec.fns[args[0]](&exec, args), nil
+	}
+	return 127, CmdNotFound
+}
+
+func (w wr_t) out(str string, a ...any) {
+	fmt.Fprintf(w.stdout, str, a...)
+}
+
+func date(exec *Executor, args []string) (int) {
+	wr := exec.wr
+	wr.out("foo\n")
+	return 0
+}
