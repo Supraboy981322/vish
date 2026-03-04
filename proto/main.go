a1310f0f8f96556b1d320cddb491b627b533e1e6
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-03-03 22:58:04 -0600

    proto date commnand

--- proto/main.go
+++ proto/main.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"os"
 	"fmt"
+	"time"
 	"errors"
 )
 
@@ -83,9 +84,40 @@ func (exec Executor) Exec(args []string) (int, error) {
 func (w wr_t) out(str string, a ...any) {
 	fmt.Fprintf(w.stdout, str, a...)
 }
+func (w wr_t) err(str string, a ...any) {
+	fmt.Fprintf(w.stderr, str, a...)
+}
 
 func date(exec *Executor, args []string) (int) {
 	wr := exec.wr
-	wr.out("foo\n")
-	return 0
+	var res, format string
+	var cur time.Time
+	var e error
+	switch len(args) {
+	 case 3:
+		cur, e = time.Parse(args[1], args[2])
+		if e != nil {
+			wr.err("%v\n", e)
+			return 2
+		}
+		fallthrough
+	 case 2:
+		format = args[1]
+		goto format
+	 case 1:
+		res = time.Now().Format(time.UnixDate)
+		goto done
+	 default:
+		panic("NO ARGS")
+	}
+	
+	format: {
+		if cur.IsZero() { cur = time.Now() }
+		res = cur.Format(format)
+	}
+
+	done: {
+		wr.out("%s", res)
+		return 0
+	}
 }
